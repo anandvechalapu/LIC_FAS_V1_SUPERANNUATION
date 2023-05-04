b8eb54e669a37b40b971fb488a6aa3b9577c375c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:35:06 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/controller/ClaimMbrController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/controller/ClaimMbrController.java
@@ -1,25 +1,44 @@
-package com.lic.epgs.claim.controller;
+package com.lic.epgs.claim.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.claim.model.ClaimMbr;
-import com.lic.epgs.claim.service.ClaimMbrService;
+
+@Repository
+public interface ClaimMbrRepository extends JpaRepository<ClaimMbr, Long> {
+
+	@Query("SELECT mbr FROM ClaimMbr mbr WHERE mbr.masterPolicyNo = ?1")
+	List<ClaimMbr> getBatchAssociateData(String masterPolicyNo);
+
+}
+
+package com.lic.epgs.claim.controller;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lic.epgs.claim.model.ClaimMbr;
+import com.lic.epgs.claim.service.ClaimMbrService;
 
 @RestController
-@RequestMapping("/claim-mbr")
+@RequestMapping("/claimMbr")
 public class ClaimMbrController {
+	
+	@Autowired
+	private ClaimMbrService claimMbrService;
+	
+	@GetMapping("/{masterPolicyNo}")
+	public List<ClaimMbr> getBatchAssociateData(@PathVariable String masterPolicyNo) {
+		return claimMbrService.getBatchAssociateData(masterPolicyNo);
+	}
 
-    @Autowired
-    private ClaimMbrService claimMbrService;
-
-    @GetMapping("/download/{batchId}/{fileType}")
-    public List<ClaimMbr> downloadClaimBulkRecords(@PathVariable String batchId,
-                                                   @PathVariable String fileType) {
-        return claimMbrService.downloadClaimBulkRecords(batchId, fileType);
-    }
 }
