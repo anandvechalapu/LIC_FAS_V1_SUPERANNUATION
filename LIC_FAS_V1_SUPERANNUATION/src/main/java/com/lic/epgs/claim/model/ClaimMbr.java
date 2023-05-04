3fdf6d4c2a9239c7492d1184c517e31b9185cf86
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:34:10 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/model/ClaimMbr.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/model/ClaimMbr.java
@@ -1,49 +1,53 @@
 package com.lic.epgs.claim.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "ClaimMbr")
 public class ClaimMbr {
-
-    @Id
-    private Long id;
-    private String batchId;
-    private String fileType;
-    private String record;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBatchId() {
-        return batchId;
-    }
-
-    public void setBatchId(String batchId) {
-        this.batchId = batchId;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public String getRecord() {
-        return record;
-    }
-
-    public void setRecord(String record) {
-        this.record = record;
-    }
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String masterPolicyNo;
+	private String memberId;
+	private String firstName;
+	private String lastName;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMasterPolicyNo() {
+		return masterPolicyNo;
+	}
+	public void setMasterPolicyNo(String masterPolicyNo) {
+		this.masterPolicyNo = masterPolicyNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 
 }
