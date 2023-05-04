97d04d32cb9f12c59d1192fa8feadedb892a5366
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 11:31:03 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/adjustmentcontribution/repository/AdjustmentContributionTempRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/adjustmentcontribution/repository/AdjustmentContributionTempRepository.java
@@ -3,11 +3,17 @@ package com.lic.epgs.adjustmentcontribution.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.adjustmentcontribution.entity.AdjustmentContributionTempEntity;
+import com.lic.epgs.adjustmentcontribution.model.AdjustmentContributionTempEntity;
 
 @Repository
 public interface AdjustmentContributionTempRepository extends JpaRepository<AdjustmentContributionTempEntity, Long> {
 
-    AdjustmentContributionTempEntity findByBatchIdAndIsActiveTrue(String batchId);
-
+  AdjustmentContributionTempEntity findByBatchIdAndIsActiveTrue(String batchId);
+  
+  void deleteByBatchIdAndIsActiveTrue(String batchId);
+  
+  void updateTotalContributionAndEmployeeContributionAndEmployerContributionAndVoluntaryContributionByBatchIdAndIsActiveTrue(String batchId, Double totalContribution, Double employeeContribution, Double employerContribution, Double voluntaryContribution);
+  
+  void updateIsActiveByBatchIdAndIsActiveTrue(String batchId, Boolean isActive);
+  
 }
