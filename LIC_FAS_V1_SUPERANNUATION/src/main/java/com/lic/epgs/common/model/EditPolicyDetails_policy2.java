1a265a3903bb8905cde3c1d2cd9b6e797f770791
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 14:57:09 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/model/EditPolicyDetails_policy2.java
@@ -0,0 +1,166 @@
+package com.lic.epgs.common.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="edit_policy_details_policy2")
+public class EditPolicyDetails_policy2 {
+
+    @Id
+    private Long id;
+    private String policyId;
+    private String policyName;
+    private String policyDescription;
+    private String policyStatus;
+    private String policyType;
+    private String policyCategory;
+    private String policySubCategory;
+    private String policyVersion;
+    private String effectiveDate;
+    private String expiryDate;
+    private String createdBy;
+    private String modifiedBy;
+    private String createdDate;
+    private String modifiedDate;
+    private String deletedBy;
+    private String deletedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPolicyId() {
+        return policyId;
+    }
+
+    public void setPolicyId(String policyId) {
+        this.policyId = policyId;
+    }
+
+    public String getPolicyName() {
+        return policyName;
+    }
+
+    public void setPolicyName(String policyName) {
+        this.policyName = policyName;
+    }
+
+    public String getPolicyDescription() {
+        return policyDescription;
+    }
+
+    public void setPolicyDescription(String policyDescription) {
+        this.policyDescription = policyDescription;
+    }
+
+    public String getPolicyStatus() {
+        return policyStatus;
+    }
+
+    public void setPolicyStatus(String policyStatus) {
+        this.policyStatus = policyStatus;
+    }
+
+    public String getPolicyType() {
+        return policyType;
+    }
+
+    public void setPolicyType(String policyType) {
+        this.policyType = policyType;
+    }
+
+    public String getPolicyCategory() {
+        return policyCategory;
+    }
+
+    public void setPolicyCategory(String policyCategory) {
+        this.policyCategory = policyCategory;
+    }
+
+    public String getPolicySubCategory() {
+        return policySubCategory;
+    }
+
+    public void setPolicySubCategory(String policySubCategory) {
+        this.policySubCategory = policySubCategory;
+    }
+
+    public String getPolicyVersion() {
+        return policyVersion;
+    }
+
+    public void setPolicyVersion(String policyVersion) {
+        this.policyVersion = policyVersion;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public String getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(String deletedDate) {
+        this.deletedDate = deletedDate;
+    }
+
+}
