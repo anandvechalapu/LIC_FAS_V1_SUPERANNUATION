38d9874a8024c8ebffd1659b62267bcddd290e03
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 17:17:26 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/policy/model/Policy.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/policy/model/Policy.java
@@ -7,76 +7,14 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="policy")
+@Table(name = "policy")
 public class Policy {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long policyId;
+    private Long id;
+
+    // other fields
     
-    private String policyName;
-    private String policyType;
-    private String policyNumber;
-    private String policyHolderName;
-    private String policyHolderAddress;
-    private String policyPremium;
-    
-    public Policy() {
-    }
-    
-    public Long getPolicyId() {
-        return policyId;
-    }
-    
-    public void setPolicyId(Long policyId) {
-        this.policyId = policyId;
-    }
-    
-    public String getPolicyName() {
-        return policyName;
-    }
-    
-    public void setPolicyName(String policyName) {
-        this.policyName = policyName;
-    }
-    
-    public String getPolicyType() {
-        return policyType;
-    }
-    
-    public void setPolicyType(String policyType) {
-        this.policyType = policyType;
-    }
-    
-    public String getPolicyNumber() {
-        return policyNumber;
-    }
-    
-    public void setPolicyNumber(String policyNumber) {
-        this.policyNumber = policyNumber;
-    }
-    
-    public String getPolicyHolderName() {
-        return policyHolderName;
-    }
-    
-    public void setPolicyHolderName(String policyHolderName) {
-        this.policyHolderName = policyHolderName;
-    }
-    
-    public String getPolicyHolderAddress() {
-        return policyHolderAddress;
-    }
-    
-    public void setPolicyHolderAddress(String policyHolderAddress) {
-        this.policyHolderAddress = policyHolderAddress;
-    }
-    
-    public String getPolicyPremium() {
-        return policyPremium;
-    }
-    
-    public void setPolicyPremium(String policyPremium) {
-        this.policyPremium = policyPremium;
-    }
+    // getters and setters
 }
