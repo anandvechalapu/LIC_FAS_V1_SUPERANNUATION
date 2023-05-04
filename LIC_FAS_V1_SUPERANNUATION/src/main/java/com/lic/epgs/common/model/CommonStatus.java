33ba67ec481311c42368c44a23b615e6934cea53
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:41:57 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/model/CommonStatus.java
@@ -0,0 +1,72 @@
+package com.lic.epgs.common.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CommonStatus {
+    @Id
+    private Long id;
+    private String code;
+    private String type;
+    private String description;
+    private String description1;
+    private String name;
+    private boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription1() {
+        return description1;
+    }
+
+    public void setDescription1(String description1) {
+        this.description1 = description1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
