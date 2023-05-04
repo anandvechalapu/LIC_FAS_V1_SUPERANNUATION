eb58801de81901953a906b3bacd8a76fea804383
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:47:09 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/model/CommonStatusModel.java
@@ -0,0 +1,17 @@
+package com.lic.epgs.common.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CommonStatusModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private boolean isActive;
+    //getters and setters
+
+}
