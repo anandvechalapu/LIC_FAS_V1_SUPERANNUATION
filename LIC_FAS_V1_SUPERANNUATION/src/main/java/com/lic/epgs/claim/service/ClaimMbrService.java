18f9d64d1eb6619de98535dfd84cd831ee561ee5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:34:26 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/service/ClaimMbrService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/service/ClaimMbrService.java
@@ -1,19 +1,21 @@
 package com.lic.epgs.claim.service;
 
-import com.lic.epgs.claim.model.ClaimMbr;
-import com.lic.epgs.claim.repository.ClaimMbrRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.claim.model.ClaimMbr;
+import com.lic.epgs.claim.repository.ClaimMbrRepository;
 
 @Service
 public class ClaimMbrService {
 
-    @Autowired
-    private ClaimMbrRepository claimMbrRepository;
+	@Autowired
+	private ClaimMbrRepository claimMbrRepository;
+	
+	public List<ClaimMbr> getBatchAssociateData(String masterPolicyNo) {
+		return claimMbrRepository.getBatchAssociateData(masterPolicyNo);
+	}
 
-    public List<ClaimMbr> downloadClaimBulkRecords(String batchId, String fileType) {
-        return claimMbrRepository.downloadClaimBulkRecords(batchId, fileType);
-    }
 }
