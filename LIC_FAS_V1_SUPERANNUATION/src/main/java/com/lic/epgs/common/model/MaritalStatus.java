cbf87e121477b46b23fcaef3bc0eb3a41d0bdb98
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:03:18 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/model/MaritalStatus.java
@@ -0,0 +1,47 @@
+package com.lic.epgs.common.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "marital_status")
+public class MaritalStatus {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "status")
+    private boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
