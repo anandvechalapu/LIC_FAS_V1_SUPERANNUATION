d81a13f6f8ad94bf43c1eb3d0825f836c62d5c98
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 11:19:35 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/adjustmentcontribution/controller/AdjustmentContributionController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/adjustmentcontribution/controller/AdjustmentContributionController.java
@@ -1,54 +1,26 @@
 package com.lic.epgs.adjustmentcontribution.controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.lic.epgs.adjustmentcontribution.model.AdjustmentContributionBulkResponseDto;
-import com.lic.epgs.adjustmentcontribution.service.BulkUploadAdjustmentContributionService;
+import com.lic.epgs.adjustmentcontribution.entity.AdjustmentContributionTempEntity;
+import com.lic.epgs.adjustmentcontribution.service.AdjustmentContributionService;
 
 @RestController
-@RequestMapping("/api/v1/adjustmentContribution")
+@RequestMapping("/adjustmentcontribution")
 public class AdjustmentContributionController {
 
     @Autowired
-    private BulkUploadAdjustmentContributionService bulkUploadAdjustmentContributionService;
-
-    @PostMapping("/bulkupload")
-    public AdjustmentContributionBulkResponseDto bulkUploadAdjustmentContribution(@RequestParam(value="masterPolicyId") Long masterPolicyId,
-                                                                                @RequestParam(value="tempPolicyId") Long tempPolicyId,
-                                                                                @RequestParam(value="createdBy") String createdBy,
-                                                                                @RequestParam(value="adjustmentContributionId") String adjustmentContributionId,
-                                                                                @RequestParam(value="unitCode") String unitCode,
-                                                                                @RequestParam(value="file") MultipartFile file){
-        return bulkUploadAdjustmentContributionService.findByMasterPolicyIdAndTempPolicyIdAndCreatedByAndAdjustmentContributionIdAndUnitCodeAndFile(masterPolicyId, tempPolicyId, createdBy, adjustmentContributionId, unitCode, file);
-    }
-
-    @GetMapping("/usedLicIds")
-    public List<AdjustmentContributionBulkResponseDto> usedLicIds(@RequestParam(value="alreadyUsedLicIdList") List<String> alreadyUsedLicIdList){
-        return bulkUploadAdjustmentContributionService.findByAlreadyUsedLicIdList(alreadyUsedLicIdList);
-    }
-
-    @GetMapping("/errors")
-    public List<AdjustmentContributionBulkResponseDto> errors(@RequestParam(value="errorList") List<String> errorList){
-        return bulkUploadAdjustmentContributionService.findByErrorList(errorList);
-    }
-
-    @GetMapping("/success")
-    public List<AdjustmentContributionBulkResponseDto> success(@RequestParam(value="successList") List<String> successList){
-        return bulkUploadAdjustmentContributionService.findBySuccessList(successList);
-    }
-
-    @GetMapping("/transaction")
-    public AdjustmentContributionBulkResponseDto transaction(@RequestParam(value="transactionStatus") String transactionStatus,
-                                                            @RequestParam(value="transactionMessage") String transactionMessage){
-        return bulkUploadAdjustmentContributionService.findByTransactionStatusAndTransactionMessage(transactionStatus, transactionMessage);
+    private AdjustmentContributionService adjustmentContributionService;
+    
+    @GetMapping("/get")
+    public Optional<AdjustmentContributionTempEntity> getByBatchIdAndIsActiveTrue(@RequestParam("batchId") String batchId) {
+        return adjustmentContributionService.findByBatchIdAndIsActiveTrue(batchId);
     }
 
 }
