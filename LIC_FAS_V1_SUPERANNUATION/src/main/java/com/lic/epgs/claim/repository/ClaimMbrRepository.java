d19ea35ab5b83a4e781e7c171a9a51b38527d6e2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:33:44 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/repository/ClaimMbrRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/claim/repository/ClaimMbrRepository.java
@@ -1,17 +1,17 @@
 package com.lic.epgs.claim.repository;
 
-import com.lic.epgs.claim.model.ClaimMbr;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.lic.epgs.claim.model.ClaimMbr;
 
 @Repository
 public interface ClaimMbrRepository extends JpaRepository<ClaimMbr, Long> {
 
-    @Query(value = "SELECT * FROM claimmbr WHERE batch_id = :batchId and file_type = :fileType", nativeQuery = true)
-    List<ClaimMbr> downloadClaimBulkRecords(@Param("batchId") String batchId, @Param("fileType") String fileType);
+	@Query("SELECT c FROM ClaimMbr c WHERE c.masterPolicyNo = :masterPolicyNo")
+	public List<ClaimMbr> getBatchAssociateData(String masterPolicyNo);
 
 }
