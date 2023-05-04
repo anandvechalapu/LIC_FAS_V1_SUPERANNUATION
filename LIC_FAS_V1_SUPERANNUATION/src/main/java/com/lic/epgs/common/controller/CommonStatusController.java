adbd6971f8c336b85a5e12a0670d4bfdb932c761
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:49:36 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/controller/CommonStatusController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/common/controller/CommonStatusController.java
@@ -1,29 +1,25 @@
-//Spring Boot Controller Class
 package com.lic.epgs.common.controller;
-
-import com.lic.epgs.common.dto.CommonResponseDto;
-import com.lic.epgs.common.entity.CommonStatusEntity;
-import com.lic.epgs.common.service.CommonStatusService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lic.epgs.common.model.CommonStatusModel;
+import com.lic.epgs.common.service.CommonStatusService;
+
 @RestController
+@RequestMapping("/commonStatuses")
 public class CommonStatusController {
-
-    @Autowired
-    private CommonStatusService commonStatusService;
-
-    @GetMapping("/getCommonStatus")
-    public CommonResponseDto getCommonStatus() {
-        return commonStatusService.getCommonStatus();
-    }
-
-    @GetMapping("/findAll")
-    public List<CommonStatusEntity> findAll() {
-        return commonStatusService.findAll();
-    }
+	
+	@Autowired
+	private CommonStatusService commonStatusService;
+	
+	@GetMapping
+	public List<CommonStatusModel> getAllActiveCommonStatuses() {
+		return commonStatusService.getAllActiveCommonStatuses();
+	}
 
 }
