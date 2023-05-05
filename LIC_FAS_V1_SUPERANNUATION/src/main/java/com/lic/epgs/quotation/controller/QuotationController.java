db2e837fa22bac6e78cbd893c805609eeb5fde59
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:05:56 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,26 +1,29 @@
 package com.lic.epgs.quotation.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lic.epgs.quotation.entity.CommonDocsTempEntity;
+import com.lic.epgs.quotation.dto.CommonChallanDto;
+import com.lic.epgs.quotation.dto.QuotationApiResponseDto;
 import com.lic.epgs.quotation.service.QuotationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/quotation")
 public class QuotationController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuotationController.class);
 
     @Autowired
-    QuotationService quotationService;
-    
-    @GetMapping("/{quotationId}/document")
-    public List<CommonDocsTempEntity> getDocumentDetails(@PathVariable Long quotationId) {
-        return quotationService.getDocumentDetails(quotationId);
+    private QuotationService quotationService;
+
+    @RequestMapping(value = "/getChellanNumber", method = RequestMethod.POST)
+    public QuotationApiResponseDto getChellanNumber(@RequestBody CommonChallanDto commonChallanDto) {
+        LOGGER.info("getChellanNumber method started");
+        QuotationApiResponseDto response = quotationService.getChellanNumber(commonChallanDto);
+        LOGGER.info("getChellanNumber method ended");
+        return response;
     }
-    
 }
