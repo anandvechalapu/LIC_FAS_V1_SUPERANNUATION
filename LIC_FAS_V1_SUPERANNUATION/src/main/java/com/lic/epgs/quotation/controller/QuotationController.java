62658a8bb3a0650703d925ef27aab346e69001a4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:10:01 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,40 +1,44 @@
 package com.lic.epgs.quotation.controller;
 
-import com.lic.epgs.quotation.dto.QuotationApiResponseDto;
-import com.lic.epgs.quotation.dto.QuotationDto;
-import com.lic.epgs.quotation.service.QuotationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lic.epgs.quotation.dto.QuotationDto;
+import com.lic.epgs.quotation.service.GetQuotationQuotationRepository;
 
 @RestController
-@RequestMapping("/quotation")
+@RequestMapping("/quotations")
 public class QuotationController {
-
+    
     @Autowired
-    private QuotationService quotationService;
-
+    private GetQuotationQuotationRepository quotationRepository;
+    
+    @GetMapping
+    public List<QuotationDto> getAllQuotations() {
+        return quotationRepository.findAllQuotations();
+    }
+    
+    @DeleteMapping("/{quotationNumber}")
+    public void deleteQuotation(@PathVariable String quotationNumber) {
+        quotationRepository.deleteByQuotationNumber(quotationNumber);
+    }
+    
+    @PutMapping
+    public void updateQuotation(@RequestBody QuotationDto quotationDto) {
+        quotationRepository.updateQuotation(quotationDto.getQuotationNumber(), quotationDto.getProposalNumber(), quotationDto.getUnitCode());
+    }
+    
     @PostMapping
-    public ResponseEntity<QuotationApiResponseDto> saveQuotation(@RequestBody QuotationDto quotationDto){
-        return new ResponseEntity<>(quotationService.saveQuotation(quotationDto), HttpStatus.CREATED);
+    public void createQuotation(@RequestBody QuotationDto quotationDto) {
+        quotationRepository.createQuotation(quotationDto.getQuotationNumber(), quotationDto.getProposalNumber(), quotationDto.getUnitCode());
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<QuotationDto>> findById(@PathVariable Long id) {
-        return new ResponseEntity<>(quotationService.findById(id), HttpStatus.OK);
-    }
-
-    @GetMapping("/active/{proposalNumber}")
-    public ResponseEntity<Boolean> isActivePolicy(@PathVariable String proposalNumber) {
-        return new ResponseEntity<>(quotationService.isActivePolicy(proposalNumber), HttpStatus.OK);
-    }
-
-    @GetMapping("/limit/{quotationNumber}")
-    public ResponseEntity<Boolean> isMaxLimitExceeded(@PathVariable String quotationNumber) {
-        return new ResponseEntity<>(quotationService.isMaxLimitExceeded(quotationNumber), HttpStatus.OK);
-    }
-
 }
