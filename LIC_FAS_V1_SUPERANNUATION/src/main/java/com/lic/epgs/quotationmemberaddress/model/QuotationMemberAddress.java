6345e48d04d9b981979a7e20cc96ec60013a21d2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:45:05 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/model/QuotationMemberAddress.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/model/QuotationMemberAddress.java
@@ -1,34 +1,90 @@
-package com.lic.epgs.quotationmemberaddress.model;
+package com.lic.epgs.quotationmemberaddress.model; 
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity
 @Table(name = "quotation_member_address")
 public class QuotationMemberAddress {
-
-    @Id
-    private Long quotationMemberAddressId;
-
-    @NotNull
-    private Long quotationMemberId;
-
-    public Long getQuotationMemberAddressId() {
-        return quotationMemberAddressId;
-    }
-
-    public void setQuotationMemberAddressId(Long quotationMemberAddressId) {
-        this.quotationMemberAddressId = quotationMemberAddressId;
-    }
-
-    public Long getQuotationMemberId() {
-        return quotationMemberId;
-    }
-
-    public void setQuotationMemberId(Long quotationMemberId) {
-        this.quotationMemberId = quotationMemberId;
-    }
-
+ 
+ @Id
+ @Column(name="quotation_member_id")
+ private Long quotationMemberId; 
+  
+ @Column(name="address_line_1")
+ private String addressLine1;
+ 
+ @Column(name="address_line_2")
+ private String addressLine2;
+ 
+ @Column(name="city")
+ private String city;
+ 
+ @Column(name="state")
+ private String state;
+ 
+ @Column(name="zip_code")
+ private String zipCode;
+ 
+ @Column(name="country")
+ private String country;
+ 
+ public Long getQuotationMemberId() {
+  return quotationMemberId;
+ }
+ 
+ public void setQuotationMemberId(Long quotationMemberId) {
+  this.quotationMemberId = quotationMemberId;
+ }
+ 
+ public String getAddressLine1() {
+  return addressLine1;
+ }
+ 
+ public void setAddressLine1(String addressLine1) {
+  this.addressLine1 = addressLine1;
+ }
+ 
+ public String getAddressLine2() {
+  return addressLine2;
+ }
+ 
+ public void setAddressLine2(String addressLine2) {
+  this.addressLine2 = addressLine2;
+ }
+ 
+ public String getCity() {
+  return city;
+ }
+ 
+ public void setCity(String city) {
+  this.city = city;
+ }
+ 
+ public String getState() {
+  return state;
+ }
+ 
+ public void setState(String state) {
+  this.state = state;
+ }
+ 
+ public String getZipCode() {
+  return zipCode;
+ }
+ 
+ public void setZipCode(String zipCode) {
+  this.zipCode = zipCode;
+ }
+ 
+ public String getCountry() {
+  return country;
+ }
+ 
+ public void setCountry(String country) {
+  this.country = country;
+ }
+ 
 }
