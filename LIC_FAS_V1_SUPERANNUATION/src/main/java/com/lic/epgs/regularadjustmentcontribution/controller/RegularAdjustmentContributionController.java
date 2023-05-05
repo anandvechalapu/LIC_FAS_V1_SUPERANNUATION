d239a600249ba26f0fdd6c1756810a3a9c57a1df
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 14:23:37 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/controller/RegularAdjustmentContributionController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/controller/RegularAdjustmentContributionController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.regularadjustmentcontribution.model.RegularAdjustmentContribution;
+import com.lic.epgs.regularadjustmentcontribution.entity.RegularAdjustmentContribution;
 import com.lic.epgs.regularadjustmentcontribution.service.RegularAdjustmentContributionService;
 
 @RestController
@@ -19,73 +21,23 @@ public class RegularAdjustmentContributionController {
     private RegularAdjustmentContributionService regularAdjustmentContributionService;
 
     @GetMapping
-    public List<RegularAdjustmentContribution> getAllRegularAdjustmentContributions() {
-        return regularAdjustmentContributionService.getAllRegularAdjustmentContributions();
+    public List<RegularAdjustmentContribution> getAllAdjustmentContributions() {
+        return regularAdjustmentContributionService.getAllAdjustmentContributions();
+    }
+    
+    @GetMapping("/{id}")
+    public RegularAdjustmentContribution getAdjustmentContributionById(@PathVariable Long id) {
+        return regularAdjustmentContributionService.getAdjustmentContributionById(id);
     }
 
-    @GetMapping("/{masterPolicyId}")
-    public RegularAdjustmentContribution findByMasterPolicyId(@PathVariable String masterPolicyId) {
-        return regularAdjustmentContributionService.findByMasterPolicyId(masterPolicyId);
+    @PostMapping
+    public void saveAdjustmentContribution(@RequestBody RegularAdjustmentContribution regularAdjustmentContribution) {
+        regularAdjustmentContributionService.saveAdjustmentContribution(regularAdjustmentContribution);
     }
 
-    @GetMapping("/{temporaryPolicyId}")
-    public RegularAdjustmentContribution findByTemporaryPolicyId(@PathVariable String temporaryPolicyId) {
-        return regularAdjustmentContributionService.findByTemporaryPolicyId(temporaryPolicyId);
+    @GetMapping("/batch/{batchId}")
+    public void removeAdjustmentContributionBatch(@PathVariable Long batchId) {
+        regularAdjustmentContributionService.removeAdjustmentContributionBatch(batchId);
     }
 
-    @GetMapping("/{createdBy}")
-    public RegularAdjustmentContribution findByCreatedBy(@PathVariable String createdBy) {
-        return regularAdjustmentContributionService.findByCreatedBy(createdBy);
-    }
-
-    @GetMapping("/{regularContributionId}")
-    public RegularAdjustmentContribution findByRegularContributionId(@PathVariable String regularContributionId) {
-        return regularAdjustmentContributionService.findByRegularContributionId(regularContributionId);
-    }
-
-    @GetMapping("/{unitCode}")
-    public RegularAdjustmentContribution findByUnitCode(@PathVariable String unitCode) {
-        return regularAdjustmentContributionService.findByUnitCode(unitCode);
-    }
-
-    @GetMapping("/{fileName}")
-    public RegularAdjustmentContribution findByFileName(@PathVariable String fileName) {
-        return regularAdjustmentContributionService.findByFileName(fileName);
-    }
-
-    @GetMapping("/{totalCount}")
-    public RegularAdjustmentContribution findByTotalCount(@PathVariable int totalCount) {
-        return regularAdjustmentContributionService.findByTotalCount(totalCount);
-    }
-
-    @GetMapping("/{isActive}")
-    public RegularAdjustmentContribution findByIsActive(@PathVariable boolean isActive) {
-        return regularAdjustmentContributionService.findByIsActive(isActive);
-    }
-
-    @GetMapping("/{createdOn}")
-    public RegularAdjustmentContribution findByCreatedOn(@PathVariable String createdOn) {
-        return regularAdjustmentContributionService.findByCreatedOn(createdOn);
-    }
-
-    @GetMapping("/{successCount}")
-    public RegularAdjustmentContribution findBySuccessCount(@PathVariable int successCount) {
-        return regularAdjustmentContributionService.findBySuccessCount(successCount);
-    }
-
-    @GetMapping("/{failedCount}")
-    public RegularAdjustmentContribution findByFailedCount(@PathVariable int failedCount) {
-        return regularAdjustmentContributionService.findByFailedCount(failedCount);
-    }
-
-    @GetMapping("/{errorMessages}")
-    public RegularAdjustmentContribution findByErrorMessages(@PathVariable String errorMessages) {
-        return regularAdjustmentContributionService.findByErrorMessages(errorMessages);
-    }
-
-    @GetMapping("/{transactionMessage}")
-    public RegularAdjustmentContribution findByTransactionMessage(@PathVariable String transactionMessage) {
-        return regularAdjustmentContributionService.findByTransactionMessage(transactionMessage);
-    }
-
-    @GetMapping("/{transactionStatus
+}
