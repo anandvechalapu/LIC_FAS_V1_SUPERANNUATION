6f0de177cf07d8c736484bcad6e4cbcccc886271
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:49:55 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/controller/QuotationMemberAddressController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/controller/QuotationMemberAddressController.java
@@ -1,22 +1,42 @@
 package com.lic.epgs.quotationmemberaddress.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.quotationmemberaddress.dto.QuotationMemberAddressApiResponseDto;
-import com.lic.epgs.quotationmemberaddress.service.GetQuotationMemberAddressesService;
+import com.lic.epgs.quotationmemberaddress.model.QuotationMemberAddress;
+import com.lic.epgs.quotationmemberaddress.service.QuotationMemberAddressService;
 
 @RestController
+@RequestMapping("/quotationmemberaddress")
 public class QuotationMemberAddressController {
-    
-    @Autowired
-    private GetQuotationMemberAddressesService getQuotationMemberAddressesService;
-    
-    @GetMapping("/quotation-member-addresses/{quotationMemberId}")
-    public QuotationMemberAddressApiResponseDto getAll(@PathVariable Long quotationMemberId) {
-        return getQuotationMemberAddressesService.getAll(quotationMemberId);
-    }
+
+	@Autowired
+	private QuotationMemberAddressService quotationMemberAddressService;
+
+	@PostMapping
+	public ResponseEntity<QuotationMemberAddress> saveQuotationMemberAddress(
+			@RequestBody QuotationMemberAddress quotationMemberAddress) {
+		return new ResponseEntity<>(quotationMemberAddressService.saveQuotationMemberAddress(quotationMemberAddress),
+				HttpStatus.OK);
+	}
+
+	@GetMapping("/{addressId}")
+	public ResponseEntity<QuotationMemberAddress> getQuotationMemberAddress(@PathVariable Long addressId) {
+		return new ResponseEntity<>(quotationMemberAddressService.getQuotationMemberAddress(addressId), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{addressId}")
+	public ResponseEntity<?> deleteQuotationMemberAddress(@PathVariable Long addressId) {
+		quotationMemberAddressService.deleteQuotationMemberAddress(addressId);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
