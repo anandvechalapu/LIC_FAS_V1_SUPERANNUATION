7ce5d8c844f999992f3820e5b0f45ca873f86749
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:12:52 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/model/QuotationMemberAppointeeTempEntity.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/model/QuotationMemberAppointeeTempEntity.java
@@ -2,39 +2,38 @@ package com.lic.epgs.quotationmemberappointee.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "quotation_member_appointee_temp")
 public class QuotationMemberAppointeeTempEntity {
 
-	@Id
-	private Long id;
-	private Long appointeeId;
-	private Long quotationMemberId;
+    @Id
+    private Long appointeeId;
+    private Long quotationId;
+    private Long memberId;
+    private Long appointeeId;
+    
+    public Long getAppointeeId() {
+        return appointeeId;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setAppointeeId(Long appointeeId) {
+        this.appointeeId = appointeeId;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getQuotationId() {
+        return quotationId;
+    }
 
-	public Long getAppointeeId() {
-		return appointeeId;
-	}
+    public void setQuotationId(Long quotationId) {
+        this.quotationId = quotationId;
+    }
 
-	public void setAppointeeId(Long appointeeId) {
-		this.appointeeId = appointeeId;
-	}
+    public Long getMemberId() {
+        return memberId;
+    }
 
-	public Long getQuotationMemberId() {
-		return quotationMemberId;
-	}
-
-	public void setQuotationMemberId(Long quotationMemberId) {
-		this.quotationMemberId = quotationMemberId;
-	}
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
 
 }
