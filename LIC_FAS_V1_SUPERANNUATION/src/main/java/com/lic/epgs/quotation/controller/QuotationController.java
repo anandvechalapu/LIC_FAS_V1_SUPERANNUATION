dfbc5e574737f2d59b8fb850d4161eb7b0a13437
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:56:29 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,31 +1,26 @@
 package com.lic.epgs.quotation.controller;
 
-import com.lic.epgs.quotation.model.Quotation;
-import com.lic.epgs.quotation.service.RemoveDocumentByremoveDocumentIdQuotationService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lic.epgs.quotation.entity.CommonDocsTempEntity;
+import com.lic.epgs.quotation.service.QuotationService;
 
 @RestController
 @RequestMapping("/quotation")
 public class QuotationController {
 
     @Autowired
-    private RemoveDocumentByremoveDocumentIdQuotationService removeDocumentByremoveDocumentIdQuotationService;
-
-    @GetMapping("/{docId}/{quotationId}")
-    public Quotation findByDocIdAndQuotationIdAndIsActiveTrue(@PathVariable String docId, @PathVariable String quotationId) {
-        return removeDocumentByremoveDocumentIdQuotationService.findByDocIdAndQuotationIdAndIsActiveTrue(docId, quotationId);
+    QuotationService quotationService;
+    
+    @GetMapping("/{quotationId}/document")
+    public List<CommonDocsTempEntity> getDocumentDetails(@PathVariable Long quotationId) {
+        return quotationService.getDocumentDetails(quotationId);
     }
-
-    @PostMapping("/{quotationId}")
-    public List<Quotation> findAllByQuotationIdAndIsActiveTrue(@PathVariable String quotationId){
-        return removeDocumentByremoveDocumentIdQuotationService.findAllByQuotationIdAndIsActiveTrue(quotationId);
-    }
-
+    
 }
