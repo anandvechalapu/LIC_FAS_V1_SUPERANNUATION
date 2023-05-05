4f9d28d631d73e39e4d647a2d5dd4620610598d9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:31:58 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/controller/QuotationMemberBankDetailController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/controller/QuotationMemberBankDetailController.java
@@ -1,10 +1,15 @@
 package com.lic.epgs.quotationmemberbankdetail.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +17,36 @@ import com.lic.epgs.quotationmemberbankdetail.model.QuotationMemberBankDetail;
 import com.lic.epgs.quotationmemberbankdetail.service.QuotationMemberBankDetailService;
 
 @RestController
-@RequestMapping("/quotationmemberbankdetails")
+@RequestMapping("/api/quotationmemberbankdetails")
 public class QuotationMemberBankDetailController {
-
-	@Autowired
-	private QuotationMemberBankDetailService service;
 	
-	@GetMapping("/{quotationMemberId}")
-	public Optional<QuotationMemberBankDetail> getQuotationMemberBankDetail(@PathVariable Long quotationMemberId) {
-		return service.getQuotationMemberBankDetail(quotationMemberId);
+	@Autowired
+	private QuotationMemberBankDetailService quotationMemberBankDetailService;
+	
+	@GetMapping("/")
+	public List<QuotationMemberBankDetail> getAllQuotationMemberBankDetails(){
+		return quotationMemberBankDetailService.getAllQuotationMemberBankDetails();
+	}
+	
+	@GetMapping("/{bankId}")
+	public Optional<QuotationMemberBankDetail> getQuotationMemberBankDetailById(@PathVariable Long bankId){
+		return quotationMemberBankDetailService.getQuotationMemberBankDetailById(bankId);
+	}
+	
+	@PostMapping("/")
+	public QuotationMemberBankDetail saveQuotationMemberBankDetail(@RequestBody QuotationMemberBankDetail quotationMemberBankDetail) {
+		return quotationMemberBankDetailService.saveQuotationMemberBankDetail(quotationMemberBankDetail);
+	}
+	
+	@PutMapping("/{bankId}")
+	public QuotationMemberBankDetail updateQuotationMemberBankDetail(@RequestBody QuotationMemberBankDetail quotationMemberBankDetail, @PathVariable Long bankId) {
+		quotationMemberBankDetail.setBankId(bankId);
+		return quotationMemberBankDetailService.saveQuotationMemberBankDetail(quotationMemberBankDetail);
+	}
+	
+	@DeleteMapping("/{bankId}")
+	public void deleteQuotationMemberBankDetailById(@PathVariable Long bankId) {
+		quotationMemberBankDetailService.deleteQuotationMemberBankDetailById(bankId);
 	}
 
 }
