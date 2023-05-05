f6706347bf2dcb592ea2ef55c14d0669cdf4ad66
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:59:06 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmembernominee/repository/QuotationMemberNomineeRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmembernominee/repository/QuotationMemberNomineeRepository.java
@@ -1,17 +1,19 @@
 package com.lic.epgs.quotationmembernominee.repository;
 
+import com.lic.epgs.quotationmembernominee.model.QuotationMemberNominee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.quotationmembernominee.entity.QuotationMemberNominee;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface QuotationMemberNomineeRepository extends JpaRepository<QuotationMemberNominee, Long> {
 
+    @Transactional
     @Modifying
-    @Query("DELETE FROM QuotationMemberNominee q WHERE q.nomineeId = :nomineeId")
-    public void deleteQuotationMemberNominee(Long nomineeId);
+    @Query(value = "DELETE FROM QuotationMemberNominee WHERE id = :nomineeId ", nativeQuery = true)
+    void deleteQuotationMemberNominee(@Param("nomineeId") Long nomineeId);
 
 }
