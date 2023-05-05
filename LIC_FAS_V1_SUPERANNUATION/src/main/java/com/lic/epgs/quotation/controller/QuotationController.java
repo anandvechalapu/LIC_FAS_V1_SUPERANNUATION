ce65956617762fa91afeae8c155a87b09bfffdf1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:19:20 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -3,24 +3,27 @@ package com.lic.epgs.quotation.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.epgs.quotation.model.Quotation;
 import com.lic.epgs.quotation.service.QuotationService;
 
 @RestController
-@RequestMapping("/quotation")
 public class QuotationController {
 	
 	@Autowired
 	private QuotationService quotationService;
 	
-	@GetMapping("/{status}")
-	public Optional<Quotation> getQuotationByStatus(@PathVariable String status) {
-		return quotationService.getQuotationByStatus(status);
+	@GetMapping("/quotations/{proposalNumber}")
+	public ResponseEntity<Quotation> getQuotationByProposalNumber(@PathVariable String proposalNumber){
+		Optional<Quotation> quotation = quotationService.findByProposalNumber(proposalNumber);
+		if(quotation.isPresent()) {
+			return ResponseEntity.ok(quotation.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
