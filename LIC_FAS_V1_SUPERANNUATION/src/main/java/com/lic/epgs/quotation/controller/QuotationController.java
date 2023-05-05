7940f7bcf6f0b4a305914db7418b1de2258d6d5a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:11:44 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,29 +1,32 @@
+package com.lic.epgs.quotation.repository; 
+
+import com.lic.epgs.quotation.dto.QuotationApiResponseDto; 
+import com.lic.epgs.quotation.dto.QuotationDto; 
+
+public interface ActivateQuotationRepository { 
+
+  QuotationApiResponseDto activateQuotation(QuotationDto quotationDto); 
+
+}
+
 package com.lic.epgs.quotation.controller;
 
-import com.lic.epgs.quotation.dto.CommonChallanDto;
 import com.lic.epgs.quotation.dto.QuotationApiResponseDto;
-import com.lic.epgs.quotation.service.QuotationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.lic.epgs.quotation.dto.QuotationDto;
+import com.lic.epgs.quotation.service.ActivateQuotationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/quotation")
 public class QuotationController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(QuotationController.class);
 
     @Autowired
-    private QuotationService quotationService;
+    ActivateQuotationService activateQuotationService;
 
-    @RequestMapping(value = "/getChellanNumber", method = RequestMethod.POST)
-    public QuotationApiResponseDto getChellanNumber(@RequestBody CommonChallanDto commonChallanDto) {
-        LOGGER.info("getChellanNumber method started");
-        QuotationApiResponseDto response = quotationService.getChellanNumber(commonChallanDto);
-        LOGGER.info("getChellanNumber method ended");
-        return response;
+    @PostMapping("/activateQuotation")
+    public QuotationApiResponseDto activateQuotation(@RequestBody QuotationDto quotationDto) {
+        return activateQuotationService.activateQuotation(quotationDto);
     }
 }
