cdcc9554028b888adce03fdc1b05741bbc53bdc4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:46:57 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
@@ -1,21 +1,27 @@
+package com.lic.epgs.regularadjustmentcontribution.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.regularadjustmentcontribution.model.RegularAdjustmentContribution;
+import com.lic.epgs.regularadjustmentcontribution.model.BulkUploadRegularAdjustmentContribution;
 
-public interface BulkUploadRegularAdjustmentContributionRepository extends JpaRepository<RegularAdjustmentContribution, Long> {
+@Repository
+public interface BulkUploadRegularAdjustmentContributionRepository extends JpaRepository<BulkUploadRegularAdjustmentContribution, Long>  {
+	
+	@Query("SELECT b FROM BulkUploadRegularAdjustmentContribution b WHERE b.masterPolicyId = ?1 AND b.temporaryPolicyId = ?2 AND b.regularContributionId = ?3 AND b.unitCode = ?4")
+	BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndTemporaryPolicyIdAndRegularContributionIdAndUnitCode(String masterPolicyId, String temporaryPolicyId, String regularContributionId, String unitCode);
+	
+	@Query("SELECT b FROM BulkUploadRegularAdjustmentContribution b WHERE b.regularContributionId = ?1")
+	BulkUploadRegularAdjustmentContribution findByRegularContributionId(String regularContributionId);
+	
+	@Query("SELECT b FROM BulkUploadRegularAdjustmentContribution b WHERE b.createdBy = ?1")
+	BulkUploadRegularAdjustmentContribution findByCreatedBy(String createdBy);
+	
+	@Query("SELECT b FROM BulkUploadRegularAdjustmentContribution b WHERE b.fileName = ?1")
+	BulkUploadRegularAdjustmentContribution findByFileName(String fileName);
+	
+	@Query("SELECT b FROM BulkUploadRegularAdjustmentContribution b WHERE b.isActive = ?1")
+	BulkUploadRegularAdjustmentContribution findByIsActive(Boolean isActive);
 
-    List<RegularAdjustmentContribution> findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, String temporaryPolicyId, String createdBy, String regularContributionId, String unitCode, String fileName);
-
-    List<RegularAdjustmentContribution> findByMasterPolicyIdAndUnitCodeAndLICIdsIn(String masterPolicyId, String unitCode, List<Long> LICIds);
-
-    List<RegularAdjustmentContribution> findByMasterPolicyIdAndUnitCodeAndClaimStatusIn(String masterPolicyId, String unitCode, List<String> claimStatus);
-
-    List<RegularAdjustmentContribution> findByRegularContributionId(String regularContributionId);
-
-    List<RegularAdjustmentContribution> findBySuccessDataSetAndStatus(List<RegularAdjustmentContribution> successDataSet, String status);
-
-    List<RegularAdjustmentContribution> findByFailedDataSetAndStatus(List<RegularAdjustmentContribution> failedDataSet, String status);
-
-    RegularAdjustmentContribution save(RegularAdjustmentContribution regularAdjustmentContribution);
-    
 }
