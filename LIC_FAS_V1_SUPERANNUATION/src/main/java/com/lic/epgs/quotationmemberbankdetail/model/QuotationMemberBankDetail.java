7c45b113a4a0e80e61d5a025f9ac6265a2cb44d9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:31:01 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/model/QuotationMemberBankDetail.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/model/QuotationMemberBankDetail.java
@@ -1,94 +1,75 @@
 package com.lic.epgs.quotationmemberbankdetail.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "quotation_member_bank_details")
 public class QuotationMemberBankDetail {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
+	private Long bankId;
 	
-	@Column(name = "quotation_member_id")
-	private Long quotationMemberId;
-	
-	@Column(name = "bank_name")
 	private String bankName;
-	
-	@Column(name = "branch_name")
+	private String accountNumber;
+	private String accountType;
+	private String ifscCode;
 	private String branchName;
 	
-	@Column(name = "ifsc_code")
-	private String ifscCode;
-	
-	@Column(name = "account_name")
-	private String accountName;
-	
-	@Column(name = "account_number")
-	private String accountNumber;
-	
-	public Long getId() {
-		return id;
+	public QuotationMemberBankDetail() {
+		
 	}
-	
-	public void setId(Long id) {
-		this.id = id;
+
+	public Long getBankId() {
+		return bankId;
 	}
-	
-	public Long getQuotationMemberId() {
-		return quotationMemberId;
+
+	public void setBankId(Long bankId) {
+		this.bankId = bankId;
 	}
-	
-	public void setQuotationMemberId(Long quotationMemberId) {
-		this.quotationMemberId = quotationMemberId;
-	}
-	
+
 	public String getBankName() {
 		return bankName;
 	}
-	
+
 	public void setBankName(String bankName) {
 		this.bankName = bankName;
 	}
-	
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public String getIfscCode() {
+		return ifscCode;
+	}
+
+	public void setIfscCode(String ifscCode) {
+		this.ifscCode = ifscCode;
+	}
+
 	public String getBranchName() {
 		return branchName;
 	}
-	
+
 	public void setBranchName(String branchName) {
 		this.branchName = branchName;
 	}
 	
-	public String getIfscCode() {
-		return ifscCode;
-	}
 	
-	public void setIfscCode(String ifscCode) {
-		this.ifscCode = ifscCode;
-	}
-	
-	public String getAccountName() {
-		return accountName;
-	}
-	
-	public void setAccountName(String accountName) {
-		this.accountName = accountName;
-	}
-	
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-	
-	public void setAccountNumber(String accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-	
-	
+
 }
