7d130090960e57b36f2072bc9d16cc0cc54cfaa2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:49:25 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,43 +1,31 @@
-package com.lic.epgs.quotation.controller; 
+package com.lic.epgs.quotation.controller;
 
-import com.lic.epgs.quotation.model.QuotationController; 
-import com.lic.epgs.quotation.service.QuotationService; 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.RequestBody; 
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RestController; 
+import com.lic.epgs.quotation.model.Quotation;
+import com.lic.epgs.quotation.service.RemoveDocumentByremoveDocumentIdQuotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-@RequestMapping("/quotation") 
-public class QuotationController { 
+import java.util.List;
 
-    @Autowired 
-    private QuotationService quotationService; 
+@RestController
+@RequestMapping("/quotation")
+public class QuotationController {
 
-    @PostMapping("/upload") 
-    public void uploadDocument(@RequestBody QuotationController quotationController) { 
-        quotationService.uploadDocument(); 
-    } 
+    @Autowired
+    private RemoveDocumentByremoveDocumentIdQuotationService removeDocumentByremoveDocumentIdQuotationService;
 
-    @PostMapping("/save") 
-    public void saveDocument(@RequestBody QuotationController quotationController) { 
-        quotationService.saveDocument(); 
-    } 
+    @GetMapping("/{docId}/{quotationId}")
+    public Quotation findByDocIdAndQuotationIdAndIsActiveTrue(@PathVariable String docId, @PathVariable String quotationId) {
+        return removeDocumentByremoveDocumentIdQuotationService.findByDocIdAndQuotationIdAndIsActiveTrue(docId, quotationId);
+    }
 
-    @PostMapping("/logStartEnd") 
-    public void logStartEndUploadDocument(@RequestBody QuotationController quotationController) { 
-        quotationService.logStartEndUploadDocument(); 
-    } 
-
-    @PostMapping("/handleExceptions") 
-    public void handleExceptionsGracefully(@RequestBody QuotationController quotationController) { 
-        quotationService.handleExceptionsGracefully(); 
-    } 
-
-    @PostMapping("/returnErrorMessage") 
-    public void returnAppropriateErrorMessage(@RequestBody QuotationController quotationController) { 
-        quotationService.returnAppropriateErrorMessage(); 
-    } 
+    @PostMapping("/{quotationId}")
+    public List<Quotation> findAllByQuotationIdAndIsActiveTrue(@PathVariable String quotationId){
+        return removeDocumentByremoveDocumentIdQuotationService.findAllByQuotationIdAndIsActiveTrue(quotationId);
+    }
 
 }
