1d17267d7132d6487219fc3b135fc9fd34a5932d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:24:36 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
@@ -1,12 +1,21 @@
 package com.lic.epgs.regularadjustmentcontribution.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.lic.epgs.regularadjustmentcontribution.model.BulkUploadRegularAdjustmentContribution;
 
+@Repository
 public interface BulkUploadRegularAdjustmentContributionRepository extends JpaRepository<BulkUploadRegularAdjustmentContribution, Long> {
-    BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, String temporaryPolicyId, String createdBy, String regularContributionId, String unitCode, String fileName);
-    BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndLICIdsAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, String LICIds, String createdBy, String regularContributionId, String unitCode, String fileName);
-    BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndUnitCodeAndFileName(String masterPolicyId, String unitCode, String fileName);
-    BulkUploadRegularAdjustmentContribution findByRegularContributionIdAndFileName(String regularContributionId, String fileName);
-    BulkUploadRegularAdjustmentContribution findBySuccessDataAndFailedDataAndErrorMessagesAndTransactionMessageAndTransactionStatus(List<String> successData, List<String> failedData, List<String> errorMessages, String transactionMessage, String transactionStatus);
+	
+	BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, String temporaryPolicyId, String createdBy, String regularContributionId, String unitCode, String fileName);
+	
+	BulkUploadRegularAdjustmentContribution findByRegularContributionId(String regularContributionId);
+	
+	BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndUnitCode(String masterPolicyId, String unitCode);
+	
+	BulkUploadRegularAdjustmentContribution findByLicIdsAndMasterPolicyIdAndExistingClaims(String licIds, String masterPolicyId, String existingClaims);
+	
+	BulkUploadRegularAdjustmentContribution findBySuccessDataSetAndFailedDataSetAndErrorMessagesAndTransactionMessageAndTransactionStatus(String successDataSet, String failedDataSet, String errorMessages, String transactionMessage, String transactionStatus);
+	
 }
