f20e436710e69efc016c6bbd3621cd613700ac0e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:05:32 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationService.java
@@ -1,20 +1,25 @@
 package com.lic.epgs.quotation.service;
 
-import java.util.List;
-
+import com.lic.epgs.quotation.dto.CommonChallanDto;
+import com.lic.epgs.quotation.dto.QuotationApiResponseDto;
+import com.lic.epgs.quotation.repository.QuotationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.quotation.entity.CommonDocsTempEntity;
-import com.lic.epgs.quotation.repository.QuotationRepository;
-
 @Service
 public class QuotationService {
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuotationService.class);
+
     @Autowired
-    QuotationRepository quotationRepository;
-    
-    public List<CommonDocsTempEntity> getDocumentDetails(Long quotationId) {
-        return quotationRepository.getDocumentDetails(quotationId);
+    private QuotationRepository quotationRepository;
+
+    public QuotationApiResponseDto getChellanNumber(CommonChallanDto commonChallanDto) {
+        LOGGER.info("getChellanNumber method started");
+        QuotationApiResponseDto response = quotationRepository.getChellanNumber(commonChallanDto);
+        LOGGER.info("getChellanNumber method ended");
+        return response;
     }
+    
 }
