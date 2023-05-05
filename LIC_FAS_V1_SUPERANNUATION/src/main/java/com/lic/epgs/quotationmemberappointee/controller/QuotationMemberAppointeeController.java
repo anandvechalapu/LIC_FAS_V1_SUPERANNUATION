7d922d5e57047c44daea936702a2de137c2c0bc7
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:13:32 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/controller/QuotationMemberAppointeeController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/controller/QuotationMemberAppointeeController.java
@@ -1,26 +1,32 @@
 package com.lic.epgs.quotationmemberappointee.controller;
 
-import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.quotationmemberappointee.model.QuotationMemberAppointee;
+import com.lic.epgs.quotationmemberappointee.model.QuotationMemberAppointeeTempEntity;
 import com.lic.epgs.quotationmemberappointee.service.QuotationMemberAppointeeService;
 
 @RestController
-@RequestMapping("/quotationmemberappointee")
+@RequestMapping("/quotationMemberAppointee")
 public class QuotationMemberAppointeeController {
-	
-	@Autowired
-	private QuotationMemberAppointeeService quotationMemberAppointeeService;
-	
-	@GetMapping("/{quotationMemberId}")
-	public List<QuotationMemberAppointee> getQuotationMemberAppointeesByQuotationMemberId(@PathVariable Long quotationMemberId) {
-		return quotationMemberAppointeeService.getQuotationMemberAppointeesByQuotationMemberId(quotationMemberId);
-	}
+
+    @Autowired
+    private QuotationMemberAppointeeService quotationMemberAppointeeService;
+
+    @DeleteMapping("/delete")
+    public void deleteQuotationMemberAppointee(Long appointeeId) {
+        quotationMemberAppointeeService.deleteQuotationMemberAppointee(appointeeId);
+    }
+
+    @PostMapping("/save")
+    public QuotationMemberAppointeeTempEntity saveQuotationMemberAppointee(@RequestBody @Valid QuotationMemberAppointeeTempEntity quotationMemberAppointeeTempEntity) {
+        return quotationMemberAppointeeService.saveQuotationMemberAppointee(quotationMemberAppointeeTempEntity);
+    }
 
 }
