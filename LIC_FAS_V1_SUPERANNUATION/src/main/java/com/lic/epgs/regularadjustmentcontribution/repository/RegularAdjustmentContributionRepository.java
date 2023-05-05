5ee570d79fa3f4bf1d7f6cc25f6b0e2ff26e368b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 14:21:56 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/RegularAdjustmentContributionRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/RegularAdjustmentContributionRepository.java
@@ -1,25 +1,15 @@
-package com.lic.epgs.regularadjustmentcontribution.repository; 
+package com.lic.epgs.regularadjustmentcontribution.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
-import org.springframework.stereotype.Repository; 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.regularadjustmentcontribution.model.RegularAdjustmentContribution; 
+import com.lic.epgs.regularadjustmentcontribution.entity.RegularAdjustmentContribution;
 
 @Repository
 public interface RegularAdjustmentContributionRepository extends JpaRepository<RegularAdjustmentContribution, Long> {
 
-    RegularAdjustmentContribution findByMasterPolicyId(String masterPolicyId);
-    RegularAdjustmentContribution findByTemporaryPolicyId(String temporaryPolicyId);
-    RegularAdjustmentContribution findByCreatedBy(String createdBy);
-    RegularAdjustmentContribution findByRegularContributionId(String regularContributionId);
-    RegularAdjustmentContribution findByUnitCode(String unitCode);
-    RegularAdjustmentContribution findByFileName(String fileName);
-    RegularAdjustmentContribution findByTotalCount(int totalCount);
-    RegularAdjustmentContribution findByIsActive(boolean isActive);
-    RegularAdjustmentContribution findByCreatedOn(String createdOn);
-    RegularAdjustmentContribution findBySuccessCount(int successCount);
-    RegularAdjustmentContribution findByFailedCount(int failedCount);
-    RegularAdjustmentContribution findByErrorMessages(String errorMessages);
-    RegularAdjustmentContribution findByTransactionMessage(String transactionMessage);
-    RegularAdjustmentContribution findByTransactionStatus(String transactionStatus);
+    @Query(value = "DELETE FROM regular_adjustment_contribution WHERE batch_id = ?1", nativeQuery = true)
+    void removeAdjustmentContributionBatch(Long batchId);
+
 }
