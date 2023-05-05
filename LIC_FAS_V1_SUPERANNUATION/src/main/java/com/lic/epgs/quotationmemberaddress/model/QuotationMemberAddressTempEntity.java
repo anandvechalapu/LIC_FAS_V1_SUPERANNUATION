ebb9db306af122026b569f505df33ad5d3025e55
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:34:54 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/model/QuotationMemberAddressTempEntity.java
@@ -0,0 +1,58 @@
+package com.lic.epgs.quotationmemberaddress.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="quotation_member_address_temp")
+public class QuotationMemberAddressTempEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="member_id")
+    private Long memberId;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="status")
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
