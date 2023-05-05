236405e7b70791fe66469db6ce4191bd9bfd6458
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:31:22 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/service/QuotationMemberBankDetailService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/service/QuotationMemberBankDetailService.java
@@ -1,21 +1,34 @@
 package com.lic.epgs.quotationmemberbankdetail.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lic.epgs.quotationmemberbankdetail.model.QuotationMemberBankDetail;
-import com.lic.epgs.quotationmemberbankdetail.repository.GetQuotationMemberBankDetailsRepository;
+import com.lic.epgs.quotationmemberbankdetail.repository.QuotationMemberBankDetailRepository;
 
 @Service
 public class QuotationMemberBankDetailService {
-
-	@Autowired
-	private GetQuotationMemberBankDetailsRepository repository;
 	
-	public Optional<QuotationMemberBankDetail> getQuotationMemberBankDetail(Long quotationMemberId){
-		return repository.findByQuotationMemberId(quotationMemberId);
+	@Autowired
+	private QuotationMemberBankDetailRepository quotationMemberBankDetailRepository;
+	
+	public List<QuotationMemberBankDetail> getAllQuotationMemberBankDetails(){
+		return quotationMemberBankDetailRepository.findAll();
+	}
+	
+	public Optional<QuotationMemberBankDetail> getQuotationMemberBankDetailById(Long bankId){
+		return quotationMemberBankDetailRepository.findById(bankId);
+	}
+	
+	public QuotationMemberBankDetail saveQuotationMemberBankDetail(QuotationMemberBankDetail quotationMemberBankDetail) {
+		return quotationMemberBankDetailRepository.save(quotationMemberBankDetail);
+	}
+	
+	public void deleteQuotationMemberBankDetailById(Long bankId) {
+		quotationMemberBankDetailRepository.deleteById(bankId);
 	}
 
 }
