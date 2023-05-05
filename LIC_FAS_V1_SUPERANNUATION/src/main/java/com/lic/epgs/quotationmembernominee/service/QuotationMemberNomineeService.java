7bcdd601a534edd40b29284b3306d73c21f44775
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:00:11 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmembernominee/service/QuotationMemberNomineeService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmembernominee/service/QuotationMemberNomineeService.java
@@ -1,19 +1,22 @@
 package com.lic.epgs.quotationmembernominee.service;
 
+import com.lic.epgs.quotationmembernominee.model.QuotationMemberNominee;
 import com.lic.epgs.quotationmembernominee.repository.QuotationMemberNomineeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class QuotationMemberNomineeService {
 
     @Autowired
-    private QuotationMemberNomineeRepository quotationMemberNomineeRepository;
+    QuotationMemberNomineeRepository quotationMemberNomineeRepository;
 
-    public ResponseEntity<Map<String, Object>> getAll(Long memberId) {
-        return quotationMemberNomineeRepository.getAll(memberId);
+    public void deleteQuotationMemberNominee(Long nomineeId) {
+        quotationMemberNomineeRepository.deleteQuotationMemberNominee(nomineeId);
     }
+
+    public QuotationMemberNominee saveQuotationMemberNominee(QuotationMemberNominee quotationMemberNominee) {
+        return quotationMemberNomineeRepository.save(quotationMemberNominee);
+    }
+
 }
