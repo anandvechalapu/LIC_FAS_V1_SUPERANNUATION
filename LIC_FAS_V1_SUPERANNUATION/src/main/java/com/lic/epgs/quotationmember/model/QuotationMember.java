c422a2e44573a23eac736d70de057c7578c563ee
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:38:59 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmember/model/QuotationMember.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmember/model/QuotationMember.java
@@ -1,54 +1,55 @@
-package com.lic.epgs.quotationmember.model;
-
-import javax.persistence.*;
-
 @Entity
 @Table(name = "quotation_member")
 public class QuotationMember {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "batch_id")
-    private String batchId;
-
+    
     @Column(name = "quotation_id")
     private String quotationId;
+    
+    @Column(name = "batch_id")
+    private String batchId;
+    
+    // other fields
+ 
+    // getters and setters
+}
 
-    @Column(name = "contribution")
-    private String contribution;
+package com.lic.epgs.quotationmember.model;
 
-    public Long getId() {
-        return id;
-    }
+import javax.persistence.*;
+import java.util.List;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBatchId() {
-        return batchId;
-    }
-
-    public void setBatchId(String batchId) {
-        this.batchId = batchId;
-    }
-
-    public String getQuotationId() {
-        return quotationId;
-    }
-
-    public void setQuotationId(String quotationId) {
-        this.quotationId = quotationId;
-    }
-
-    public String getContribution() {
-        return contribution;
-    }
-
-    public void setContribution(String contribution) {
-        this.contribution = contribution;
-    }
-
+@Entity
+@Table(name = "batches")
+public class Batch {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "batch_id")
+    private String batchId;
+    
+    @Column(name = "created_on")
+    private String createdOn;
+    
+    @Column(name = "file_name")
+    private String fileName;
+    
+    @Column(name = "total_count")
+    private Integer totalCount;
+    
+    @Column(name = "success_count")
+    private Integer successCount;
+    
+    @Column(name = "failed_count")
+    private Integer failedCount;
+    
+    @Column(name = "error_info")
+    private List<String> errorInfo;
+    
+    // getters and setters
 }
