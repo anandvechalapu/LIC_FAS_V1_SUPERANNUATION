6732a50741d8ef2c42066fcfab17f380e10e6b49
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:13:09 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/service/QuotationMemberAppointeeService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/service/QuotationMemberAppointeeService.java
@@ -1,21 +1,23 @@
 package com.lic.epgs.quotationmemberappointee.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.quotationmemberappointee.model.QuotationMemberAppointee;
+import com.lic.epgs.quotationmemberappointee.QuotationMemberAppointeeTempEntity;
 import com.lic.epgs.quotationmemberappointee.repository.QuotationMemberAppointeeRepository;
 
 @Service
 public class QuotationMemberAppointeeService {
-	
-	@Autowired
-	private QuotationMemberAppointeeRepository quotationMemberAppointeeRepository;
-	
-	public List<QuotationMemberAppointee> getQuotationMemberAppointeesByQuotationMemberId(Long quotationMemberId) {
-		return quotationMemberAppointeeRepository.getQuotationMemberAppointeesByQuotationMemberId(quotationMemberId);
-	}
+
+    @Autowired
+    private QuotationMemberAppointeeRepository quotationMemberAppointeeRepository;
+
+    public void deleteQuotationMemberAppointee(Long appointeeId) {
+        quotationMemberAppointeeRepository.deleteQuotationMemberAppointee(appointeeId);
+    }
+
+    public QuotationMemberAppointeeTempEntity saveQuotationMemberAppointee(QuotationMemberAppointeeTempEntity quotationMemberAppointeeTempEntity) {
+        return quotationMemberAppointeeRepository.save(quotationMemberAppointeeTempEntity);
+    }
 
 }
