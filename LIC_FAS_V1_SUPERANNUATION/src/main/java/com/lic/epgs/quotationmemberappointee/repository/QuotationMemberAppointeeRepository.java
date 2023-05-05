48a6b248ba87e80f68e028b669df288b7c2cffc5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:12:39 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/repository/QuotationMemberAppointeeRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberappointee/repository/QuotationMemberAppointeeRepository.java
@@ -1,14 +1,17 @@
 package com.lic.epgs.quotationmemberappointee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.quotationmemberappointee.model.QuotationMemberAppointee;
+import com.lic.epgs.quotationmemberappointee.QuotationMemberAppointeeTempEntity;
 
 @Repository
-public interface QuotationMemberAppointeeRepository extends JpaRepository<QuotationMemberAppointee, Long> {
-  
-    @Query("SELECT a FROM QuotationMemberAppointee a WHERE a.quotationMemberId = ?1")
-    public Iterable<QuotationMemberAppointee> getQuotationMemberAppointeesByQuotationMemberId(Long quotationMemberId);
+public interface QuotationMemberAppointeeRepository extends JpaRepository<QuotationMemberAppointeeTempEntity, Long> {
+
+    @Modifying
+    @Query("DELETE FROM QuotationMemberAppointeeTempEntity WHERE appointeeId = ?1")
+    void deleteQuotationMemberAppointee(Long appointeeId);
+
 }
