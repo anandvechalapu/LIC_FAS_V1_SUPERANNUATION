9bd5e00f74f53c4e7c904d60f75772da8f5cc18a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:17:13 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/controller/QuotationController.java
@@ -1,44 +1,26 @@
 package com.lic.epgs.quotation.controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.quotation.dto.QuotationDto;
-import com.lic.epgs.quotation.service.GetQuotationQuotationRepository;
+import com.lic.epgs.quotation.model.Quotation;
+import com.lic.epgs.quotation.service.QuotationService;
 
 @RestController
-@RequestMapping("/quotations")
+@RequestMapping("/quotation")
 public class QuotationController {
-    
-    @Autowired
-    private GetQuotationQuotationRepository quotationRepository;
-    
-    @GetMapping
-    public List<QuotationDto> getAllQuotations() {
-        return quotationRepository.findAllQuotations();
-    }
-    
-    @DeleteMapping("/{quotationNumber}")
-    public void deleteQuotation(@PathVariable String quotationNumber) {
-        quotationRepository.deleteByQuotationNumber(quotationNumber);
-    }
-    
-    @PutMapping
-    public void updateQuotation(@RequestBody QuotationDto quotationDto) {
-        quotationRepository.updateQuotation(quotationDto.getQuotationNumber(), quotationDto.getProposalNumber(), quotationDto.getUnitCode());
-    }
-    
-    @PostMapping
-    public void createQuotation(@RequestBody QuotationDto quotationDto) {
-        quotationRepository.createQuotation(quotationDto.getQuotationNumber(), quotationDto.getProposalNumber(), quotationDto.getUnitCode());
-    }
+	
+	@Autowired
+	private QuotationService quotationService;
+	
+	@GetMapping("/{status}")
+	public Optional<Quotation> getQuotationByStatus(@PathVariable String status) {
+		return quotationService.getQuotationByStatus(status);
+	}
+
 }
