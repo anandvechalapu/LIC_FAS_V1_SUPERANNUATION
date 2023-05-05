afd71e887d9979dbafb5a3a6ebdc96c1b7e4edbe
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:16:59 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationService.java
@@ -1,33 +1,21 @@
 package com.lic.epgs.quotation.service;
 
-import com.lic.epgs.quotation.dto.QuotationApiResponseDto;
-import com.lic.epgs.quotation.dto.QuotationDto;
-import com.lic.epgs.quotation.repository.QuotationRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import com.lic.epgs.quotation.model.Quotation;
+import com.lic.epgs.quotation.repository.QuotationTempRepository;
 
 @Service
 public class QuotationService {
+	
+	@Autowired
+	private QuotationTempRepository quotationTempRepository;
 
-    @Autowired
-    private QuotationRepository quotationRepository;
-
-    public QuotationApiResponseDto saveQuotation(QuotationDto quotationDto) {
-        return quotationRepository.saveQuotation(quotationDto);
-    }
-
-    public boolean isActivePolicy(String proposalNumber) {
-        return quotationRepository.isActivePolicy(proposalNumber);
-    }
-
-    public boolean isMaxLimitExceeded(String quotationNumber) {
-        return quotationRepository.isMaxLimitExceeded(quotationNumber);
-    }
-
-    public Optional<QuotationDto> findById(Long id) {
-        return quotationRepository.findById(id);
-    }
+	public Optional<Quotation> getQuotationByStatus(String status) {
+		return quotationTempRepository.findByStatus(status);
+	}	
 
 }
