fe6f650371381fd286dcf6e443dc30215153e9e2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:27:09 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/controller/QuotationMemberBankDetailController.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/controller/QuotationMemberBankDetailController.java
@@ -1,23 +1,26 @@
 package com.lic.epgs.quotationmemberbankdetail.controller;
 
-import com.lic.epgs.quotationmemberbankdetail.model.QuotationMemberBankDetail;
-import com.lic.epgs.quotationmemberbankdetail.service.QuotationMemberBankDetailService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lic.epgs.quotationmemberbankdetail.model.QuotationMemberBankDetail;
+import com.lic.epgs.quotationmemberbankdetail.service.QuotationMemberBankDetailService;
+
 @RestController
-@RequestMapping("/quotationMemberBankDetail")
+@RequestMapping("/quotationmemberbankdetails")
 public class QuotationMemberBankDetailController {
 
-    @Autowired
-    private QuotationMemberBankDetailService quotationMemberBankDetailService;
-
-    @GetMapping("/{bankId}/{quotationMemberId}")
-    public QuotationMemberBankDetail findByBankIdAndQuotationMemberId(@PathVariable Long bankId, @PathVariable Long quotationMemberId) {
-        return quotationMemberBankDetailService.findByBankIdAndQuotationMemberId(bankId, quotationMemberId);
-    }
+	@Autowired
+	private QuotationMemberBankDetailService service;
+	
+	@GetMapping("/{quotationMemberId}")
+	public Optional<QuotationMemberBankDetail> getQuotationMemberBankDetail(@PathVariable Long quotationMemberId) {
+		return service.getQuotationMemberBankDetail(quotationMemberId);
+	}
 
 }
