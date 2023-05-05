14822d85442091628e3e0891d0999f7decb8de6c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:14:18 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationTempService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/service/QuotationTempService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.quotation.entity.Quotation;
+import com.lic.epgs.quotation.entity.QuotationTempEntity;
 import com.lic.epgs.quotation.repository.QuotationTempRepository;
 
 @Service
@@ -14,11 +14,12 @@ public class QuotationTempService {
     @Autowired
     private QuotationTempRepository quotationTempRepository;
 
-    public Optional<Quotation> getQuotationById(int id) {
-        return quotationTempRepository.findById(id);
+    public QuotationTempEntity saveQuotationTemp(QuotationTempEntity quotationTemp) {
+        return quotationTempRepository.save(quotationTemp);
     }
 
-    public Quotation saveQuotation(Quotation quotation) {
-        return quotationTempRepository.save(quotation);
+    public Optional<QuotationTempEntity> findByQuotationId(Integer quotationId) {
+        return quotationTempRepository.findByQuotationId(quotationId);
     }
+
 }
