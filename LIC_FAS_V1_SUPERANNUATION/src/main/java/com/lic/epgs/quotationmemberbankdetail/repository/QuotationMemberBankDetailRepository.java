3ada1ed3ac985aedf3139813c549fc046a289e35
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:30:34 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/repository/QuotationMemberBankDetailRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberbankdetail/repository/QuotationMemberBankDetailRepository.java
@@ -1,14 +1,13 @@
-@Repository
-package com.lic.epgs.quotationmemberbankdetail.repository;
+package com.lic.epgs.quotationmemberbankdetail.repository; 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.quotationmemberbankdetail.model.QuotationMemberBankDetail;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 @Repository
 public interface QuotationMemberBankDetailRepository extends JpaRepository<QuotationMemberBankDetail, Long> {
+	
+	void deleteById(Long bankId);
 
-    QuotationMemberBankDetail findByBankIdAndQuotationMemberId(Long bankId, Long quotationMemberId);
 }
