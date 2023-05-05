f920c4e917e23aeff9697f464bd952f319413dda
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:51:14 +0530

    initial commit

---
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmembernominee/model/QuotationMemberNomineeDto.java
@@ -0,0 +1,40 @@
+package com.lic.epgs.quotationmembernominee.model;
+
+public class QuotationMemberNomineeDto {
+    private Long nomineeId;
+    private String firstName;
+    private String lastName;
+    private String relationship;
+
+    public Long getNomineeId() {
+        return nomineeId;
+    }
+
+    public void setNomineeId(Long nomineeId) {
+        this.nomineeId = nomineeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+}
