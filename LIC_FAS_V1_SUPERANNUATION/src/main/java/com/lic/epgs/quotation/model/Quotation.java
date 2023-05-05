4e9bdb82dd4983ce160178d89995a41ee95a57e9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 09:48:46 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/model/Quotation.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotation/model/Quotation.java
@@ -1,93 +1,20 @@
-}
-
-package com.lic.epgs.quotation.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 @Entity
-@Table(name="quotations")
-public class Quotation {
+@Table(name = "quotation")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quotation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private Long id;
-  
-  private String mphCode;
-  private String mphName;
-  private String product;
-  private String lineOfBusiness;
-  private String proposalNumber;
-  private String quotationStatus;
-  private String createdOn;
-  
-  public Quotation() {}
-  
-  public Long getId() {
-    return this.id;
-  }
-  
-  public void setId(Long id) {
-    this.id = id;
-  }
-  
-  public String getMphCode() {
-    return this.mphCode;
-  }
-  
-  public void setMphCode(String mphCode) {
-    this.mphCode = mphCode;
-  }
-  
-  public String getMphName() {
-    return this.mphName;
-  }
-  
-  public void setMphName(String mphName) {
-    this.mphName = mphName;
-  }
-  
-  public String getProduct() {
-    return this.product;
-  }
-  
-  public void setProduct(String product) {
-    this.product = product;
-  }
-  
-  public String getLineOfBusiness() {
-    return this.lineOfBusiness;
-  }
-  
-  public void setLineOfBusiness(String lineOfBusiness) {
-    this.lineOfBusiness = lineOfBusiness;
-  }
-  
-  public String getProposalNumber() {
-    return this.proposalNumber;
-  }
-  
-  public void setProposalNumber(String proposalNumber) {
-    this.proposalNumber = proposalNumber;
-  }
-  
-  public String getQuotationStatus() {
-    return this.quotationStatus;
-  }
-  
-  public void setQuotationStatus(String quotationStatus) {
-    this.quotationStatus = quotationStatus;
-  }
-  
-  public String getCreatedOn() {
-    return this.createdOn;
-  }
-  
-  public void setCreatedOn(String createdOn) {
-    this.createdOn = createdOn;
-  }
-  
+    @Column(name = "doc_id")
+    private String docId;
+
+    @Column(name = "quotation_id")
+    private String quotationId;
+    
+    @Column(name = "is_active")
+    private Boolean isActive;
 }
