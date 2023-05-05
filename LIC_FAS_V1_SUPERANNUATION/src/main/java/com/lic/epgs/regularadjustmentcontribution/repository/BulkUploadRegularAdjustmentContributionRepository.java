39455cfe995d38b4bcdd86b146c0bbd9a3d95386
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:37:26 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/repository/BulkUploadRegularAdjustmentContributionRepository.java
@@ -1,21 +1,33 @@
 package com.lic.epgs.regularadjustmentcontribution.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.lic.epgs.regularadjustmentcontribution.model.BulkUploadRegularAdjustmentContribution;
 
-@Repository
 public interface BulkUploadRegularAdjustmentContributionRepository extends JpaRepository<BulkUploadRegularAdjustmentContribution, Long> {
-	
-	BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, String temporaryPolicyId, String createdBy, String regularContributionId, String unitCode, String fileName);
-	
-	BulkUploadRegularAdjustmentContribution findByRegularContributionId(String regularContributionId);
-	
-	BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndUnitCode(String masterPolicyId, String unitCode);
-	
-	BulkUploadRegularAdjustmentContribution findByLicIdsAndMasterPolicyIdAndExistingClaims(String licIds, String masterPolicyId, String existingClaims);
-	
-	BulkUploadRegularAdjustmentContribution findBySuccessDataSetAndFailedDataSetAndErrorMessagesAndTransactionMessageAndTransactionStatus(String successDataSet, String failedDataSet, String errorMessages, String transactionMessage, String transactionStatus);
-	
+ 
+    BulkUploadRegularAdjustmentContribution findByMasterPolicyId(String masterPolicyId);
+    
+    BulkUploadRegularAdjustmentContribution findByTemporaryPolicyId(String temporaryPolicyId);
+    
+    BulkUploadRegularAdjustmentContribution findByCreatedBy(String createdBy);
+    
+    BulkUploadRegularAdjustmentContribution findByRegularContributionId(String regularContributionId);
+    
+    BulkUploadRegularAdjustmentContribution findByUnitCode(String unitCode);
+    
+    BulkUploadRegularAdjustmentContribution findByFileName(String fileName);
+    
+    BulkUploadRegularAdjustmentContribution findByTotalCount(Integer totalCount);
+    
+    BulkUploadRegularAdjustmentContribution findByIsActive(Boolean isActive);
+    
+    BulkUploadRegularAdjustmentContribution findByCreatedOn(Long createdOn);
+    
+    BulkUploadRegularAdjustmentContribution findBySuccessCount(Integer successCount);
+    
+    BulkUploadRegularAdjustmentContribution findByFailedCount(Integer failedCount);
+    
+    BulkUploadRegularAdjustmentContribution findByTransactionMessage(String transactionMessage);
+    
+    BulkUploadRegularAdjustmentContribution findByTransactionStatus(String transactionStatus);
 }
