1510c8592330d1b6e10990050bac829c71d41355
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:49:07 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/model/QuotationMemberAddress.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/quotationmemberaddress/model/QuotationMemberAddress.java
@@ -1,90 +1,88 @@
-package com.lic.epgs.quotationmemberaddress.model; 
+package com.lic.epgs.quotationmemberaddress.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Column;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "quotation_member_address")
 public class QuotationMemberAddress {
- 
- @Id
- @Column(name="quotation_member_id")
- private Long quotationMemberId; 
-  
- @Column(name="address_line_1")
- private String addressLine1;
- 
- @Column(name="address_line_2")
- private String addressLine2;
- 
- @Column(name="city")
- private String city;
- 
- @Column(name="state")
- private String state;
- 
- @Column(name="zip_code")
- private String zipCode;
- 
- @Column(name="country")
- private String country;
- 
- public Long getQuotationMemberId() {
-  return quotationMemberId;
- }
- 
- public void setQuotationMemberId(Long quotationMemberId) {
-  this.quotationMemberId = quotationMemberId;
- }
- 
- public String getAddressLine1() {
-  return addressLine1;
- }
- 
- public void setAddressLine1(String addressLine1) {
-  this.addressLine1 = addressLine1;
- }
- 
- public String getAddressLine2() {
-  return addressLine2;
- }
- 
- public void setAddressLine2(String addressLine2) {
-  this.addressLine2 = addressLine2;
- }
- 
- public String getCity() {
-  return city;
- }
- 
- public void setCity(String city) {
-  this.city = city;
- }
- 
- public String getState() {
-  return state;
- }
- 
- public void setState(String state) {
-  this.state = state;
- }
- 
- public String getZipCode() {
-  return zipCode;
- }
- 
- public void setZipCode(String zipCode) {
-  this.zipCode = zipCode;
- }
- 
- public String getCountry() {
-  return country;
- }
- 
- public void setCountry(String country) {
-  this.country = country;
- }
- 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    
+    private Long memberId;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 }
