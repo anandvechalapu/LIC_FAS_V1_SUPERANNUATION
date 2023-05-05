b91e7fffb974e3476a775a429bf1a70a29969ba4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:12:32 +0530

    Updated

--- LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/service/BulkUploadRegularAdjustmentContributionService.java
+++ LIC_FAS_V1_SUPERANNUATION/src/main/java/com/lic/epgs/regularadjustmentcontribution/service/BulkUploadRegularAdjustmentContributionService.java
@@ -1,60 +1,66 @@
 package com.lic.epgs.regularadjustmentcontribution.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.regularadjustmentcontribution.model.BulkUploadRegularAdjustmentContribution;
 import com.lic.epgs.regularadjustmentcontribution.repository.BulkUploadRegularAdjustmentContributionRepository;
 
 @Service
 public class BulkUploadRegularAdjustmentContributionService {
+	
+	private BulkUploadRegularAdjustmentContributionRepository bulkUploadRegularAdjustmentContributionRepository;
 
-    @Autowired
-    private BulkUploadRegularAdjustmentContributionRepository bulkUploadRegularAdjustmentContributionRepository;
-
-    public BulkUploadRegularAdjustmentContribution save(BulkUploadRegularAdjustmentContribution bulkUploadRegularAdjustmentContribution){
-        return bulkUploadRegularAdjustmentContributionRepository.save(bulkUploadRegularAdjustmentContribution);
+    public BulkUploadRegularAdjustmentContributionService(BulkUploadRegularAdjustmentContributionRepository bulkUploadRegularAdjustmentContributionRepository) {
+        this.bulkUploadRegularAdjustmentContributionRepository = bulkUploadRegularAdjustmentContributionRepository;
     }
 
-    public Optional<BulkUploadRegularAdjustmentContribution> findById(Long id){
-        return bulkUploadRegularAdjustmentContributionRepository.findById(id);
+    // Retrieve the policy details for the provided Master Policy ID and Unit Code
+    public List<Object> getPolicyDetails(String masterPolicyId, String unitCode) {
+        return bulkUploadRegularAdjustmentContributionRepository.getPolicyDetails(masterPolicyId, unitCode);
     }
 
-    public List<BulkUploadRegularAdjustmentContribution> findAll(){
-        return bulkUploadRegularAdjustmentContributionRepository.findAll();
+    // Check if the members with the provided LIC IDs are available for the provided Master Policy ID
+    public boolean isMembersAvailable(String masterPolicyId, List<String> licIds) {
+        return bulkUploadRegularAdjustmentContributionRepository.isMembersAvailable(masterPolicyId, licIds);
     }
 
-    public void deleteById(Long id){
-        bulkUploadRegularAdjustmentContributionRepository.deleteById(id);
+    // Check if any of the members with the provided LIC IDs have already been onboarded or have an existing claim status of Member-In Onboard, Member-In Intimation, or MemberInPayout
+    public List<Object> isMembersAlreadyOnboarded(List<String> licIds) {
+        return bulkUploadRegularAdjustmentContributionRepository.isMembersAlreadyOnboarded(licIds);
     }
 
-    public BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(String masterPolicyId, 
-    String temporaryPolicyId, String createdBy, String regularContributionId, String unitCode, String fileName){
-        return bulkUploadRegularAdjustmentContributionRepository.findByMasterPolicyIdAndTemporaryPolicyIdAndCreatedByAndRegularContributionIdAndUnitCodeAndFileName(masterPolicyId, 
-        temporaryPolicyId, createdBy, regularContributionId, unitCode, fileName);
+    // Create a new Regular Adjustment Contribution Batch and save it to the database
+    public void createRegularAdjustmentContributionBatch(String fileName, int totalCount, boolean isActive,
+			String createdBy, LocalDateTime createdOn) {
+        bulkUploadRegularAdjustmentContributionRepository.createRegularAdjustmentContributionBatch(fileName, totalCount, isActive,
+			createdBy, createdOn);
     }
 
-    public BulkUploadRegularAdjustmentContribution findByMasterPolicyIdAndUnitCodeAndFileName(String masterPolicyId, String unitCode, String fileName){
-        return bulkUploadRegularAdjustmentContributionRepository.findByMasterPolicyIdAndUnitCodeAndFileName(masterPolicyId, unitCode, fileName);
+    // Retrieve the existing record for the provided Regular Contribution ID
+    public Object getExistingRecordForRegularContributionId(String regularContributionId) {
+        return bulkUploadRegularAdjustmentContributionRepository.getExistingRecordForRegularContributionId(regularContributionId);
     }
 
-    public BulkUploadRegularAdjustmentContribution findByRegularContributionId(String regularContributionId){
-        return bulkUploadRegularAdjustmentContributionRepository.findByRegularContributionId(regularContributionId);
+    // Extract the success data from the Excel file and check if the adjustment contribution is available for each member
+    public List<Object> extractSuccessDataFromExcel(List<Object> successDataSet, List<Object> failedDataSet) {
+        return bulkUploadRegularAdjustmentContributionRepository.extractSuccessDataFromExcel(successDataSet, failedDataSet);
     }
 
-    public BulkUploadRegularAdjustmentContribution findBySuccessDataSetContainingAndFailedDataSetContaining(String successDataSet, String failedDataSet){
-        return bulkUploadRegularAdjustmentContributionRepository.findBySuccessDataSetContainingAndFailedDataSetContaining(successDataSet, failedDataSet);
+    // Set the success count and failed count of the batch based on the size of the success data set and the failed data set
+    public void setSuccessCountAndFailedCount(int successCount, int failedCount) {
+        bulkUploadRegularAdjustmentContributionRepository.setSuccessCountAndFailedCount(successCount, failedCount);
     }
 
-    public BulkUploadRegularAdjustmentContribution findByTransactionMessageAndTransactionStatus(String transactionMessage,String transactionStatus){
-        return bulkUploadRegularAdjustmentContributionRepository.findByTransactionMessageAndTransactionStatus(transactionMessage, transactionStatus);
+    // Create a list of error messages for the failed data set
+    public List<String> createErrorMessagesList(List<Object> failedDataSet) {
+        return bulkUploadRegularAdjustmentContributionRepository.createErrorMessagesList(failedDataSet);
     }
 
-    public BulkUploadRegularAdjustmentContribution findByErrorMessagesContaining(String errorMessages){
-        return bulkUploadRegularAdjustmentContributionRepository.findByErrorMessagesContaining(errorMessages);
+    // Return the response with the success data set, failed data set, error messages, transaction message, and transaction status
+    public Object returnResponse(List<Object> successDataSet, List<Object> failedDataSet, List<String> errorMessages,
+			String transactionMessage, String transactionStatus) {
+        return bulkUploadRegularAdjustmentContributionRepository.returnResponse(successDataSet, failedDataSet, errorMessages, transactionMessage, transactionStatus);
     }
-
 }
